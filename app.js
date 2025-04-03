37e9f38042adf854b920ee37e7d4b781023dd6d7
Karanpal97 <karangill1810@gmail.com>
2025-04-03 23:56:30 +0530

    feat: deployed the application on lamda functions

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
-require('dotenv').config();
-const express = require('express');
-const chalk = require('chalk');
-const setupSwagger = require('./configuration/swagger');
-const mongoose = require('mongoose');
-const authMiddleware = require('./middleware/authMiddleware')
-const userRoutes = require('./routes/userRoutes');
-const clubRoutes = require('./routes/clubRoutes');
-const bankRoutes = require('./routes/bankRoutes');
-const quizRoutes = require('./routes/quizRoutes');
-const transactionRoutes = require('./routes/transactionRoutes');
-const eventRoutes = require('./routes/eventRoutes');
-const taskRoutes = require('./routes/taskRoutes');
-const projectRoutes = require('./routes/projectRoutes');
+require("dotenv").config();
+const express = require("express");
+const chalk = require("chalk");
+const setupSwagger = require("./configuration/swagger");
+const mongoose = require("mongoose");
+const authMiddleware = require("./middleware/authMiddleware");
+const userRoutes = require("./routes/userRoutes");
+const clubRoutes = require("./routes/clubRoutes");
+const bankRoutes = require("./routes/bankRoutes");
+const quizRoutes = require("./routes/quizRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+const eventRoutes = require("./routes/eventRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+
 const app = express();
 
-// Middleware to parse JSON
-
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 app.use(express.json());
-// Allows method override via query parameter `_method`
 app.use(express.urlencoded({ extended: true }));
 
-
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log(chalk.green.bold(`✅ Connected to MongoDB at ${chalk.blue.underline(process.env.MONGO_URI)}`)))
-  .catch((err) => console.error('Error connecting to MongoDB:', err));
-// Routes
-;
-app.use('/api', userRoutes);
-app.use('/api', clubRoutes);
-app.use('/api', bankRoutes);
-app.use('/api', eventRoutes);
-app.use('/api/quiz', quizRoutes);
-app.use('/api', transactionRoutes);
-app.use('/api', taskRoutes);
-app.use('/api', projectRoutes);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() =>
+    console.log(
+      chalk.green.bold(
+        `✅ Connected to MongoDB at ${chalk.blue.underline(process.env.MONGO_URI)}`
+      )
+    )
+  )
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
+app.use("/api", userRoutes);
+app.use("/api", clubRoutes);
+app.use("/api", bankRoutes);
+app.use("/api", eventRoutes);
+app.use("/api/quiz", quizRoutes);
+app.use("/api", transactionRoutes);
+app.use("/api", taskRoutes);
+app.use("/api", projectRoutes);
 
-setupSwagger(app)
-// Start the server
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(chalk.green.bold(`🚀 Server is running on port ${chalk.blueBright.bold(PORT)}`));
-});
+setupSwagger(app);
+
+module.exports = app; 
